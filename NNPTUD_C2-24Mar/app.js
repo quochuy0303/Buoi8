2e91397caa9abd204a74eff7f5c0b3eeb765702e
VoTranQuocHuy <qhuy071003@gmail.com>
2025-03-24 14:30:43 +0700

    updated

--- NNPTUD_C2-24Mar/app.js
+++ NNPTUD_C2-24Mar/app.js
@@ -11,7 +11,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-mongoose.connect("mongodb://localhost:27017/C2");
+mongoose.connect("mongodb://localhost:27017/Product");
 mongoose.connection.on("connected",()=>{
   console.log("connected");
 })
